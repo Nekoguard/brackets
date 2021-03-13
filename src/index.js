3c8d797bad58fef77e74782faa16c0f8aeaa4de3
Nekoguard <nekoguard@protonmail.com>
2021-03-13 22:19:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+
+    // соберём пары открытых и закрытых скобок в массив
+    for (let bracketsPair of bracketsConfig) {
+        brackets.push(bracketsPair[0] + bracketsPair[1]);
+    }
+
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.includes(brackets[i]) ) {
+            str = str.replace(brackets[i], ''); // убираем скобку из строки
+            i = -1; // перееносимся на последний символ в строке, чтобы дальше проверить закрывающуюся скобку
+        }
+    }
+
+    let result = str.length === 0 ? true : false;
+
+    return result;
 }
